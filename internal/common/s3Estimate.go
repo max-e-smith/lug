a9213b9f03feaa6c5bede3f4b2b1ed6ef7d0cda2
max-e-smith <maxwellfth@gmail.com>
2026-01-11 20:27:47 -0700

    paginate getting s3 disk usage estimate

--- internal/common/s3Estimate.go
+++ internal/common/s3Estimate.go
@@ -5,26 +5,36 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/spf13/viper"
 )
 
 func GetDiskUsageEstimate(bucket string, s3client s3.Client, rootPaths []string) (int64, error) {
 	var totalSurveysSize int64 = 0
+	verbose := viper.GetBool("verbose")
 
 	for _, surveyRootPath := range rootPaths {
 		fmt.Printf("Getting disk usage estimate for s3 files on %s at %s\n", bucket, surveyRootPath)
-		// TODO paginate
-		result, err := s3client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+
+		params := &s3.ListObjectsV2Input{
 			Bucket: aws.String(bucket),
 			Prefix: aws.String(surveyRootPath),
-		})
-		if err != nil {
-			return totalSurveysSize, err
 		}
 
-		for _, object := range result.Contents {
-			//log.Printf("key=%s size=%d", aws.ToString(object.Key), *object.Size)
-			totalSurveysSize = totalSurveysSize + *object.Size
+		filePaginator := s3.NewListObjectsV2Paginator(&s3client, params)
+		for filePaginator.HasMorePages() {
+			page, err := filePaginator.NextPage(context.TODO())
+			if err != nil {
+				return totalSurveysSize, err
+			}
+
+			for _, object := range page.Contents {
+				if verbose {
+					fmt.Printf(" key=%s size=%d\n", aws.ToString(object.Key), *object.Size)
+				}
+				totalSurveysSize = totalSurveysSize + *object.Size
+			}
 		}
+
 	}
 
 	return totalSurveysSize, nil
